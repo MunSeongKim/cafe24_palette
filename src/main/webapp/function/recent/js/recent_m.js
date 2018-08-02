19f100f820ecc157fe13af8d267412518f9d2543
bit <bit@DESKTOP-76H2K2A>
2018-08-02 16:49:27 +0900

    [Qyuee's] 2018-08-02

--- src/main/webapp/function/recent/js/recent_m.js
+++ src/main/webapp/function/recent/js/recent_m.js
@@ -10,7 +10,7 @@
 			"0" : {
 				"iProductNo" : "101470",
 				"sProductName" : "첫번째 상품",
-				"sImgSrc" : "localhost:8080/mammoth/function/recent/images/recent_product1.jpg",
+				"sImgSrc" : "localhost:8080/mammoth/function/recent/recent_product1.jpg",
 				"isAdultProduct" : "F",
 				"link_product_detail" : "/product/린넨-suit-자켓/101470/display/2/",
 				"sParam" : "?product_no=101470&cate_no=39&display_group=2"
@@ -18,7 +18,7 @@
 			"1" : {
 				"iProductNo" : "98781",
 				"sProductName" : "두번째 상품",
-				"sImgSrc" : "localhost:8080/mammoth/function/recent/images/recent_product2.jpg",
+				"sImgSrc" : "localhost:8080/mammoth/function/recent/recent_product2.jpg",
 				"isAdultProduct" : "F",
 				"link_product_detail" : "/product/cb-리얼플러스-어깨패드/98781/display/2/",
 				"sParam" : "?product_no=98781&cate_no=52&display_group=2"
@@ -26,7 +26,7 @@
 			"2" : {
 				"iProductNo" : "101479",
 				"sProductName" : "세번째 상품",
-				"sImgSrc" : "localhost:8080/mammoth/function/recent/images/recent_product3.jpg",
+				"sImgSrc" : "localhost:8080/mammoth/function/recent/recent_product3.jpg",
 				"isAdultProduct" : "F",
 				"link_product_detail" : "/product/시원한-린넨-7부-셔츠/101479/display/2/",
 				"sParam" : "?product_no=101479&cate_no=2825&display_group=2"
@@ -256,7 +256,7 @@
 			var list = []; //resultDatas에 추가해줄 리스트 객체
 			
 			// 최근 본 상품 목록 갯수
-			$(".recent_count").text(Object.keys(jsonData).length);
+			$(".recent-count").text(Object.keys(jsonData).length);
 			/* front api 가상 데이터 */
 			var productDatas = tmpProductData;
 			var optDatas = tmpOptData;
@@ -328,7 +328,7 @@
 	
 })(jQuery);
 $(document).ready(function() {   
-	var dataId = $('.recent_main_div').parent().attr('id');
+	var dataId = $('.recent-main-div').parent().attr('id');
 	
 	$('.zoom-menu li[data-id='+dataId+']').click(function() {
 		$.recent.init({ 
@@ -340,7 +340,7 @@ $(document).ready(function() {
 		});
 		
 		// 최근 본 상품 링크
-		$('.recent_row_title a').attr('href', window.location.hostname+'/product/recent_view_product.html');
+		$('.recent-row-title a').attr('href', window.location.hostname+'/product/recent_view_product.html');
 		
 		$('a[data-slide="prev"]').click(function() {
 		  $('#myCarousel').carousel('prev');
